7372c432077c9f4baaf8495f748b7805f467558f
Tian Biao <btian@thoughtworks.com>
2017-03-21 19:06:50 +1100

    read Stream

---
+++ stream/readStream.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+var data = '';
+
+const readerStream = fs.createReadStream('input.txt');
+
+readerStream.setEncoding('UTF8');
+
+readerStream.on('data', function(chunk) {
+  data += chunk;
+});
+
+readerStream.on('end', function() {
+  console.log(data);
+});
+
+readerStream.on('error', function(err) {
+  console.log(err.stack);
+});
+
+console.log("program Ended");
